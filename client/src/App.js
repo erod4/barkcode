7c4ee4681fe76cf4ab38aa6c09d82f8bfbce3f58
erod4 <e.rod71@outlook.com>
2023-10-03 09:16:51 -0700

    edited client url

--- client/src/App.js
+++ client/src/App.js
@@ -23,19 +23,19 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />}></Route>
-        <Route path="#/login" element={<Login />}></Route>
-        <Route path="#/register" element={<Register />}></Route>
-        <Route path="#/location" element={<LocationRequest />}></Route>
-        <Route path="#/home" element={<Home />}></Route>
-        <Route path="#/edit-profile/:id" element={<EditPetProfile />}></Route>
-        <Route path="#/forgot-password" element={<ForgotPassword />}></Route>
-        <Route path="#/manage-pets/" element={<ManagePets />}></Route>
-        <Route path="#/resources" element={<Resources />}></Route>
-        <Route path="#/pet-profile/:id" element={<PetProfile />}></Route>
-        <Route path="#/settings" element={<Settings />}></Route>
-        <Route path="#/qr" element={<QRCode />}></Route>
-        <Route path="#/add-pet" element={<AddPet />}></Route>
-        <Route path="#/create-alert" element={<AddAlert />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/location" element={<LocationRequest />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/edit-profile/:id" element={<EditPetProfile />}></Route>
+        <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+        <Route path="/manage-pets/" element={<ManagePets />}></Route>
+        <Route path="/resources" element={<Resources />}></Route>
+        <Route path="/pet-profile/:id" element={<PetProfile />}></Route>
+        <Route path="/settings" element={<Settings />}></Route>
+        <Route path="/qr" element={<QRCode />}></Route>
+        <Route path="/add-pet" element={<AddPet />}></Route>
+        <Route path="/create-alert" element={<AddAlert />}></Route>
 
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
